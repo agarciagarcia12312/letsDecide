3c5e32093fd23e95b962d9c2c65ff5023828bb5b
Daniel Rietveld <dan.rietveld@gmail.com>
2017-09-03 21:38:48 -0600

    facebook authentication attempt

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var override = require("method-override");
 var bodyParser = require("body-parser");
 var mrHandle = require("express-handlebars");
 var routes = require("./controller/decide.js")
+var passport = require("passport");
+var FacebookStrategy = require("passport-facebook");
+var session = require('express-session');
 
 
 
@@ -42,4 +45,43 @@ db.sequelize.sync({ force: true }).then(function() {
   });
 });
 
+// facebook authentication
+// =========================================================================
 
+app.use(session({
+  secret: "dev",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/localhost:8080"));
+});
+
+  var FACEBOOK_APP_ID = '829157353919820',
+      FACEBOOK_APP_SECRET = 'ae52e4194c977e7550c0cd5279070698';
+
+    var facebookOp = {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: 'http://localhost:8080/auth/facebook/callback',
+      profileFields: ['emails']
+    };
+
+    var facebookCallback = function(accessToken, refreshToken, profile, cb){
+      console.log(accessToken, refreshToken, profile);
+    };
+
+    passport.use(new FacebookStrategy(facebookOp, facebookCallback));
+
+
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'emails' }));
+
+
+
+  app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+// =============================================================================
